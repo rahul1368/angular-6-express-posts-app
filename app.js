d4161abe73a04954c542d24fc2cd649c02c3529c
rahul <rahulchoudhary666666@gmail.com>
2018-11-22 00:59:32 +0530

    dist folder added

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, '__dirnamest')));
-app.use('/posts', express.static(path.join(__dirname, 'dist')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use('/post', post);
 app.use('/', post);
 
